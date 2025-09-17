125a065cd83b832d8d6a4c3542ba59c5d3030a6f
unknown <kimyoumin1221@gmail.com>
2025-09-17 11:50:52 +0900

    A.M class

---
+++ 25MP2/2025.09.17/pointer.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void sum(int x, int y, int* result)
+{
+    *result = x + y;
+
+}
+
+int main()
+{
+    int a = 3;
+    int b = 2;
+    int c = 0;
+
+    int* pa;
+    pa = &a;
+
+
+    sum(a, b, &c);
+    
+    printf("%d", c);
+
+    return 0;
+}
---
+++ 25MP2/참고용 코드/memo.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#include <Windows.h>
+
+#define PI 3.14159226535897
+
+void moveCursor(int x, int y)
+{
+    printf("\x1b[%d;%dH", y, x);
+}
+
+//매개변수 isExploded
+//0: 폭발전 폭탄
+//1: 폭발함
+void printBomb(int isExploded)
+{
+    //폭탄은 항상 7칸
+    if (isExploded)
+    {
+        printf("\x1b[A^^^^^^^");
+        printf("\x1b[B\x1b[7D!!BAM!!");
+        printf("\x1b[B\x1b[7D^^^^^^^");
+    }
+    else
+        printf("(  b  )");
+}
+
+int main()
+{
+    // 여기부터 코드를 작성하세요----------------------   
+
+
+
+
+    // 여기까지 코드를 작성하세요----------------------   
+    moveCursor(10, 20);
+    return 0;
+}
