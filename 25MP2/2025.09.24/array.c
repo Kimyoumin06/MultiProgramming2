b3b71bd6426bee3fd68bc6de9789768070d28a22
unknown <kimyoumin1221@gmail.com>
2025-09-24 11:43:59 +0900

    fixed Array code

--- 25MP2/2025.09.24/array.c
+++ 25MP2/2025.09.24/array.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-    char a[6] = {'L', 'E', 'S', '_', 'G', 'O'};
+    char a[6];
 
-    /*char a[0] = 'L';
-    char a[1] = 'E';
-    char a[2] = 'S';
-    char a[3] = '_';
-    char a[4] = 'G';
-    char a[5] = 'O';*/
+    a[0] = 'L';
+    a[1] = 'E';
+    a[2] = 'S';
+    a[3] = '_';
+    a[4] = 'G';
+    a[5] = 'O';
 
     int i = 0;
     for (i = 0; i < 6; i++)
